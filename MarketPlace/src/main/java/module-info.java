311148b25c46bb2eeca3614fbe36d3bb63895f0b
erikpablot28 <112137130+ErikTrivino@users.noreply.github.com>
2023-03-30 17:21:39 -0500

    Se agrega el proyecto
    Se agrega la base del proyecto

---
+++ MarketPlace/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module co.edu.uqvirtual.marketplace {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    exports co.edu.uqvirtual.marketplace.aplicacion;
+    opens co.edu.uqvirtual.marketplace.aplicacion to javafx.fxml;
+    exports co.edu.uqvirtual.marketplace.controller;
+    opens co.edu.uqvirtual.marketplace.controller to javafx.fxml;
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ProyectoProgramacion3
 En este repositorio se encuentra el proyecto de programacion 3 llamado "MarketPlace".
+
+Grupo:
+Erik Pablo Triviño Gonzalez
+Diego Alexander Jimenez Duran
+Kevin Payanene Ardila
